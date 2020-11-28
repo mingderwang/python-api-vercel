b1fbfea62f84c6fffccfdf233abe1f1fde7e2617
Mingder Wang <mingderwang@Mingders-Mac-mini.local>
2020-11-28 10:59:28 +0800

    add cors

--- api/star.py
+++ api/star.py
@@ -5,7 +5,23 @@ import random
 import uuid
 import time
 
+""" The HTTP request handler """
 class handler(BaseHTTPRequestHandler):
+
+  def _send_cors_headers(self):
+    """ Sets headers required for CORS """
+    self.send_header("Access-Control-Allow-Origin", "*")
+    self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+    self.send_header("Access-Control-Allow-Headers", "x-api-key,Content-Type")
+
+  def send_dict_response(self, d):
+    """ Sends a dictionary (JSON) back to the client """
+    self.wfile.write(bytes(dumps(d), "utf8"))
+
+  def do_OPTIONS(self):
+      self.send_response(200)
+      self._send_cors_headers()
+      self.end_headers()
 
   def do_GET(self):
     result = json.dumps(
